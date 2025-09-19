da3d959040724c7bab6e8ef2778735e3a8ccf33c
DenaBaghery <denabaghery96@gmail.com>
2025-09-19 15:31:45 +0200

    Save current changes before fixing remote repository

---
+++ src/textclassification.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from scipy import sparse
+import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import accuracy_score, f1_score, classification_report
+
+#Eine Klasse (z.B. TextClassifier) oder Funktionen für:
+class Text
+#Initialisierung (Modellwahl: KNN, MLP, etc.)
+#Training (fit)
+#Vorhersage (predict)
+#Evaluation (evaluate)
+#Optional: Methoden für das Speichern/Laden von Modellen
